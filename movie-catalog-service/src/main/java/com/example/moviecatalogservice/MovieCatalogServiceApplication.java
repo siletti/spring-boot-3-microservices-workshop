19a2b2626a9c53bcb72fd219bef3a7f5cf5201fa
Alberto Siletti <alberto.siletti@consultant.vodafoneomnitel.it>
2023-01-26 16:01:45 +0100

    a simple circuitbreaker

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -13,9 +12,12 @@ public class MovieCatalogServiceApplication {
     @Bean
     @LoadBalanced
     RestTemplate getRestTemplate() {
+/*
         HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
         clientHttpRequestFactory.setConnectTimeout(3000);
         return new RestTemplate(clientHttpRequestFactory);
+*/
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,7 +3,8 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.UserRating;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +14,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
+@RequiredArgsConstructor
 public class MovieCatalogResource {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
+    @CircuitBreaker(name = "myProjectAllRemoteCallsCB", fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
         UserRating forObject = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
-
-        return forObject.userRating().parallelStream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.movieId(), Movie.class);
 
 //            var movie = webClientBuilder.build()
 //                    .get()
@@ -33,11 +32,22 @@ public class MovieCatalogResource {
 //                    .bodyToMono(Movie.class)
 //                    .block();
 
+        return forObject.userRating().parallelStream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.movieId(), Movie.class);
+
             return CatalogItem.builder()
                     .name(movie.name())
                     .desc(movie.description())
                     .rate(rating.rating())
                     .build();
         }).toList();
+    }
+
+    public List<CatalogItem> getFallbackCatalog(@PathVariable String userId, Exception e) {
+        return List.of(CatalogItem.builder()
+                .name("No movie")
+                .desc("userid: " + userId)
+                .rate(0)
+                .build());
     }
 }
